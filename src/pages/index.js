9af5d946a73861fa333dd529a72b7992c1943535
istone-you <istoneyou1205@yahoo.co.jp>
2023-04-20 08:12:32 +0000

    レイアウト調整

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,31 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import * as styles from "../components/index.module.css"
+import '../components/styles/fade.css'
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <div className={styles.list}>
-      {data.allMicrocmsBlogs.edges.map(({ node }) => (
-        <Link to={`/blog/${node.blogsId}`} className={styles.listItem} key={node.blogsId}>
-          {node.eyecatch && (
-            <div className={styles.textCenter}>
-              <img src={node.eyecatch.url} alt={node.title} height="100px" />
-            </div>
-          )}
-          <p>{node.title}</p>
-        </Link>
-      ))}
+    <div className={styles.textCenter}>
+      <div className="Icon">
+        <h1>
+          Welcome to Yu Ishii's Blog!
+        </h1>
+      </div>
+      <div className="Comment">
+        <div className={styles.list}>
+          {data.allMicrocmsBlogs.edges.map(({ node }) => (
+            <Link to={`/blog/${node.blogsId}`} className={styles.listItem} key={node.blogsId}>
+              {node.eyecatch && (
+                <div className={styles.textCenter}>
+                  <img src={node.eyecatch.url} alt={node.title} height="100px" />
+                </div>
+              )}
+              <p>{node.title}</p>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   </Layout>
 )
